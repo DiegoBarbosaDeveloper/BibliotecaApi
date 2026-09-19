928370136a354fe6c62c6a92eb65ae319fde8244
Diego Barbosa <diegobarbosa090305@gmail.com>
2026-09-19 09:56:02 -0500

    "add: services and mapper or libros"

--- src/main/java/com/usta/biblioteca/mapper/LibroMapper.java
+++ src/main/java/com/usta/biblioteca/mapper/LibroMapper.java
@@ -1,5 +1,6 @@
 package com.usta.biblioteca.mapper;
 
+import com.usta.biblioteca.dto.LibroRequest;
 import org.mapstruct.Mapper;
 import com.usta.biblioteca.domain.Libro;
 import com.usta.biblioteca.dto.LibroResponse;
@@ -7,4 +8,5 @@ import com.usta.biblioteca.dto.LibroResponse;
 @Mapper(componentModel = "spring")
 public interface LibroMapper {
     LibroResponse toResponse(Libro entidad);
+    Libro toEntity(LibroRequest entidad);
 }
