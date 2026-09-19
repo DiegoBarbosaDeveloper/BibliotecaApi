71695244d9122fb20af12fa976b8b5fc5d6ab677
Laura Daniela Melo Baron <lauradmelo0536@gmail.com>
2026-09-19 11:32:36 -0500

    UsuarioController y UsuarioRequest

--- src/main/java/com/usta/biblioteca/dto/UsuarioRequest.java
+++ src/main/java/com/usta/biblioteca/dto/UsuarioRequest.java
@@ -1,5 +1,17 @@
 package com.usta.biblioteca.dto;
 
-public record UsuarioRequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UsuarioRequest(
     // TODO: Agregar los componentes del contrato Swagger y las validaciones correspondientes.
-}
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Size(max = 100, message = "El nombre no puede superar los 100 caracteres")
+    String nombre,
+
+    @NotBlank(message = "El correo electrónico no puede estar vacío")
+    @Email(message = "El formato del correo no es válido")
+    @Size(max = 100, message = "El correo no puede superar los 100 caracteres")
+    String email
+){}
