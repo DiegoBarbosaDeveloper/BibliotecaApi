f49ae517bddd3a6a3733dbf959c6bb5d1810a6ba
Laura Daniela Melo Baron <lauradmelo0536@gmail.com>
2026-09-19 11:04:54 -0500

    LibroController inyección del Servicio LibroService e implementación de estados. LibroRequest se realizó la entrada de datos con validaciones.

--- src/main/java/com/usta/biblioteca/dto/LibroRequest.java
+++ src/main/java/com/usta/biblioteca/dto/LibroRequest.java
@@ -1,5 +1,24 @@
 package com.usta.biblioteca.dto;
 
-public record LibroRequest() {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record LibroRequest(
     // TODO: Agregar los componentes del contrato Swagger y las validaciones correspondientes.
-}
+    @NotBlank(message = "El título no puede estar vacío")
+    @Size(max = 100, message = "El título no puede superar los 100 caracteres")
+    String titulo,
+
+    @NotBlank(message = "El autor no puede estar vacío")
+    @Size(max = 100, message = "El autor no puede superar los 100 carcateres")
+    String autor,
+
+    @NotBlank(message = "El ISBN no puede estar vacío")
+    @Size (min = 10, max = 13, message = "El ISBN debe tener entre 10 y 13 caracteres")
+    String isbn,
+
+    @NotNull(message = "El estado de disponibilidad es obligatorio")
+    Boolean disponible
+){}
--- src/main/java/com/usta/biblioteca/view/controller/LibroController.java
+++ src/main/java/com/usta/biblioteca/view/controller/LibroController.java
@@ -1,5 +1,56 @@
 package com.usta.biblioteca.view.controller;
 
+import com.usta.biblioteca.dto.LibroRequest;
+import com.usta.biblioteca.dto.LibroResponse;
+import com.usta.biblioteca.repository.LibroRepository;
+import com.usta.biblioteca.service.LibroService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping ("api/v1/libros")
+@RequiredArgsConstructor
+
 public class LibroController {
     // TODO: Agregar endpoints /api/v1, validación de entrada y delegación al servicio.
+    private final LibroService libroService;
+    private final LibroRepository libroRepository;
+
+    @GetMapping
+    public ResponseEntity <List <LibroResponse>> getAllLibros(){
+        List<LibroResponse> libros = libroService.getLibros();
+        return ResponseEntity.ok(libros);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LibroResponse>getLibroById(@PathVariable Long id){
+        LibroResponse libro = libroService.getLibro(id);
+        return ResponseEntity.ok(libro);
+    }
+
+    @PostMapping
+    public ResponseEntity<LibroResponse> createLibro(@Valid @RequestBody LibroRequest request){
+        LibroResponse nuevoLibro = libroService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoLibro);
+    }
+
+    @PutMapping("/id")
+    public ResponseEntity<LibroResponse> updateLibro(
+            @PathVariable Long id,
+            @Valid @RequestBody LibroRequest request){
+        LibroResponse libroActualizado = libroService.update(id, request);
+        return ResponseEntity.ok(libroActualizado);
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<Void> deleteLibro(@PathVariable Long id){
+        libroService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
